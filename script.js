567fd6b49200cd80d411145a731bd81fc2a834ae
TarunVanshkar <117818351+TarunVanshkar@users.noreply.github.com>
2023-04-16 17:39:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let currentLevel = document.getElementById("level");    // let or var works here because we will update its value. const wont work
+
+let level=0;
+// loop through the parent elements of the target element
+while(currentLevel.parentNode != document)
+{
+    level++;     // Increase the level
+    currentLevel = currentLevel.parentNode;     // Update current code level
+}
+
+alert("The level of the element is: "+level);
